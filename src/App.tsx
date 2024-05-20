45162a0a5ced37639f34ba3055ebdbe6fb7bd34e
Gleb Leus <lv.gleb44@gmail.com>
2024-05-20 17:29:48 +0300

    build: 20.05.2024

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Cart from './pages/Cart';
 import NotFound from './pages/404';
-import PizzaDetail from './pages/PizzaDetail';
 import MainTemplate from './templates/MainTemplate';
 
 import './scss/app.scss';
+
+const Cart = React.lazy(() => import('./pages/Cart'));
+const PizzaDetail = React.lazy(() => import('./pages/PizzaDetail'));
 
 function App() {
   return (
@@ -16,8 +17,22 @@ function App() {
       <Routes>
         <Route path="/" element={<MainTemplate />}>
           <Route path="" element={<Home />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="pizzas/:id" element={<PizzaDetail />} />
+          <Route
+            path="cart"
+            element={
+              <React.Suspense fallback={<div>Идёт загрузка корзины</div>}>
+                <Cart />
+              </React.Suspense>
+            }
+          />
+          <Route
+            path="pizzas/:id"
+            element={
+              <React.Suspense fallback={<div>Идёт загрузка питсы</div>}>
+                <PizzaDetail />
+              </React.Suspense>
+            }
+          />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
